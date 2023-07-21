3c619472dde18463b1dabdb7823e43546289ae21
Norbert Tasarz <tasarz_norbert@wp.pl>
2023-07-21 16:21:04 +0200

    Rewrited code to not give cancer while reading it, 90% done data collecting, will finish later that night

--- WebScrape.py
+++ WebScrape.py
@@ -5,7 +5,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
-from bs4 import BeautifulSoup
+
+import html
+
+def extract_numeric_value(text):
+    # Remove any non-numeric characters and spaces from the text
+    numeric_text = ''.join(filter(str.isdigit, text))
+    return int(numeric_text)
 
 class Bot:
     def __init__(self):
@@ -15,13 +21,14 @@ class Bot:
         options.add_experimental_option("excludeSwitches", ["enable-automation"])
         options.add_experimental_option('useAutomationExtension', False)
         options.add_argument("--incognito")
-        self.bot = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+        desired_version = "114.0.5735.90"  # Replace with the desired version, or wait for chromedriver to update
+        self.bot = webdriver.Chrome(service=Service(ChromeDriverManager(version=desired_version).install()), options=options)
         self.bot.get("https://it.pracuj.pl/")
         self.bot.maximize_window()
         self.current_site = 1
         self.linki_do_oferty = []
         self.dane_oferty = []
-        
+
     def click_button_acc(self):
         WebDriverWait(self.bot, 100).until(
             EC.presence_of_element_located((By.CLASS_NAME, "size-medium.variant-primary.cookies_b1fqykql"))
@@ -50,97 +57,96 @@ class Bot:
         return numer_stron_sesji
     
     # Pobieranie danych na temat ofert
-    # WILL CHANGE LATER BC LOOKS LIKE SHIT
     def get_data(self):
-        oferty = self.bot.find_elements(By.CSS_SELECTOR, 'div[class^="ContentBoxstyles__Wrapper-"]')
+        oferty = self.bot.find_elements(By.CSS_SELECTOR, 'div.ContentBoxstyles__Wrapper-sc-11jmnka-0.jevXWE.JobOfferstyles__ContentBoxWrapper-sc-1rq6ue2-0')
         self.id_oferty = 0
         self.linki_do_oferty = [None] * len(oferty)
-        
-        # Pobierz dane z ofert na głównej
+
+        # Pobierz linki ofert
         for oferta in oferty:
-            inner_html = oferta.get_attribute("innerHTML")
-            soup = BeautifulSoup(inner_html, 'html.parser')
-            dane = []
-
-            tytul_element = soup.find('h3', attrs={'data-test': 'offer-title'})
-            company_name_element = soup.find('span', attrs={'data-test': 'company-name'})
-            offer_location_element = soup.find('span', attrs={'data-test': 'offer-location'})
-            offer_management_level_element = soup.find('span', attrs={'data-test': 'offer-management-level'})
-            date = soup.find('div', class_='JobOfferstyles__FooterText-sc-1rq6ue2-22')
-            
-            tytul = tytul_element.text if tytul_element else ''
-            company = company_name_element.text if company_name_element else ''
-            location = offer_location_element.text if offer_location_element else ''
-            management_level = offer_management_level_element.text if offer_management_level_element else ''
-            p_date = date.text if date else ''
-            p_date = p_date.replace("opublikowana: ", "")
-            
-            if tytul:
-                dane = [tytul, company, location, management_level, p_date, None, None, self.id_oferty]
-                if soup.find('span', attrs={'data-test': 'offer-salary'}):
-                    salary = soup.find('span', attrs={'data-test': 'offer-salary'})
-                    dane[6] = salary.text
-
-                # Pobierz tagi
-                tagarray = []
-                if soup.find('div', attrs={'data-test': 'offer-tags'}):
-                    tagi = soup.find('div', attrs={'data-test': 'offer-tags'})
-                    inner_html = str(tagi)
-                    tag_div = BeautifulSoup(inner_html, 'html.parser')
-                    tags = tag_div.find_all(class_="Chipsstyles__Wrapper-sc-17yerqz-0 cShFq JobOfferstyles__ChipsStyled-sc-1rq6ue2-19 dDTkNx")
-                    for tag in tags:
-                        inner_text = tag.get_text(strip=True)
-                        tagarray.append(inner_text)
-                    dane[5] = tagarray
-
-                # Pobierz linki do ofert
-                try:
-                    button = oferta.find_element(By.CLASS_NAME, 'JobOfferstyles__TitleButton-sc-1rq6ue2-5.HPWqN')
-                    button.click()
-                    href_element = oferta.find_element(By.CLASS_NAME, 'OfferLocationsListstyles__LocationsItemLink-sc-b1eixg-2.ZUWyH')
-                    href = href_element.get_attribute("href")
-                    self.linki_do_oferty[self.id_oferty] = href
-                except NoSuchElementException:
-                    offer_link = soup.find('a', attrs={'data-test': 'offer-link'})
-                    if offer_link is not None:
-                        href = offer_link['href']
-                        self.linki_do_oferty[self.id_oferty] = href
-                self.dane_oferty.append(dane)
-                self.id_oferty += 1
-            
+            try:
+                button = oferta.find_element(By.CLASS_NAME, 'JobOfferstyles__TitleButton-sc-1rq6ue2-5.HPWqN')
+                button.click()
+                href_element = oferta.find_element(By.CLASS_NAME, 'OfferLocationsListstyles__LocationsItemLink-sc-b1eixg-2.ZUWyH')
+                href = href_element.get_attribute("href")
+                self.linki_do_oferty[self.id_oferty] = href
+            except NoSuchElementException:
+                offer_link_element = oferta.find_element(By.CSS_SELECTOR, 'a[data-test="offer-link"]')
+                link_text = offer_link_element.get_attribute("href")
+                if link_text is not None:
+                    self.linki_do_oferty[self.id_oferty] = link_text
+            self.id_oferty += 1
         self.id_oferty = 0
-        # wejdz w każdy self.linki_do_oferty i pobierz z niego dane i wpierdol do bot.dane_oferty, chce sie zajebać
-        # Pobierz dane ze strony oferty
+        
+        # Pobierz dane z linków do ofert
         for oferta in self.linki_do_oferty:
             if oferta is not None:
                 self.bot.get(str(oferta))
-                inner_data = [None, None, None, None, None, None, None, None]
+                inner_data = [None] * 12
                 # getting data
-                # tryb pracy?
+                # title?
+                try:
+                    title = self.bot.find_element(By.CSS_SELECTOR, 'h1.offer-viewkHIhn3[data-test="text-positionName"][data-scroll-id="job-title"]')
+                    inner_data[0] = title.get_attribute("innerHTML")
+                except NoSuchElementException:
+                    pass
+                # company?
+                try:
+                    company = self.bot.find_element(By.CSS_SELECTOR, 'h2.offer-viewwtdXJ4[data-test="text-employerName"]')
+                    company = company.get_attribute("innerHTML")
+                    index_of_first_less_than = company.find("<")
+                    company = company[:index_of_first_less_than].strip()
+                    inner_data[1] = company
+                except NoSuchElementException:
+                    pass
+                # location?
+                try:
+                    location = self.bot.find_element(By.CSS_SELECTOR, 'div.offer-viewqtkGPu[data-test="text-benefit"]')
+                    inner_data[2] = location.get_attribute("innerHTML")
+                except NoSuchElementException:
+                    pass
+                # management_level?
+                try:
+                    management_level = self.bot.find_element(By.CSS_SELECTOR, 'div.offer-viewXo2dpV[data-test="sections-benefit-employment-type-name-text"]')
+                    inner_data[3] = management_level.get_attribute("innerHTML")
+                except NoSuchElementException:
+                    pass
+                # salary?
+                try:
+                    salary = self.bot.find_element(By.CSS_SELECTOR, "strong[data-test='text-earningAmount']")
+                    salary_from = salary.find_element(By.CSS_SELECTOR, 'span[data-test="text-earningAmountValueFrom"]')
+                    salary_to = salary.find_element(By.CSS_SELECTOR, 'span[data-test="text-earningAmountValueTo"]')
+                    salary_from = extract_numeric_value(html.unescape(salary_from.get_attribute("innerHTML")))
+                    salary_to = extract_numeric_value(html.unescape(salary_to.get_attribute("innerHTML")))
+                    inner_data[4] = salary_from
+                    inner_data[5] = salary_from
+                except NoSuchElementException:
+                    pass 
+                # tryb_pracy?
                 try:
                     tryb_pracy = self.bot.find_element(By.CSS_SELECTOR, 'div.offer-viewXo2dpV[data-test="sections-benefit-work-modes-text"]')
-                    inner_data[0] = tryb_pracy.get_attribute("innerHTML")
+                    inner_data[6] = tryb_pracy.get_attribute("innerHTML")
                 except NoSuchElementException:
                     pass
                 # etat?
                 try:
                     etat = self.bot.find_element(By.CSS_SELECTOR, 'div.offer-viewXo2dpV[data-test="sections-benefit-work-schedule-text"]')
-                    inner_data[1] = etat.get_attribute("innerHTML")
+                    inner_data[7] = etat.get_attribute("innerHTML")
                 except NoSuchElementException:
                     pass
                 # kontrakt?
                 try:
                     kontrakt = self.bot.find_element(By.CSS_SELECTOR, 'div.offer-viewXo2dpV[data-test="sections-benefit-contracts-text"]')
-                    inner_data[2] = kontrakt.get_attribute("innerHTML")
+                    inner_data[8] = kontrakt.get_attribute("innerHTML")
                 except NoSuchElementException:
                     pass
                 # specjalizacja?
                 try:
                     specjalizacja = self.bot.find_element(By.CSS_SELECTOR, 'span.offer-viewPFKc0t')
-                    inner_data[3] = specjalizacja.get_attribute("innerHTML")
+                    inner_data[9] = specjalizacja.get_attribute("innerHTML")
                 except NoSuchElementException:
                     pass
-                # technologie wymagane?
+                # technologie_wymagane?
                 try:
                     div_wymagane = self.bot.find_element(By.CSS_SELECTOR, 'div.offer-viewfjH4z3[data-scroll-id="technologies-expected-1"][data-test="section-technologies-expected"]')
                     lista = div_wymagane.find_element(By.CSS_SELECTOR, "[class^='offer-viewEX0Eq-']")
@@ -150,7 +156,7 @@ class Bot:
                         link_element = element.find_element(By.TAG_NAME, 'p')
                         inner_html = link_element.get_attribute("innerHTML")
                         self.wymagane.append(inner_html)
-                    inner_data[4] = self.wymagane
+                    inner_data[10] = self.wymagane
                 except NoSuchElementException:
                     pass
                 # technologie mile widziane?
@@ -163,7 +169,7 @@ class Bot:
                         link_element = element.find_element(By.TAG_NAME, 'p')
                         inner_html = link_element.get_attribute("innerHTML")
                         self.mile_widziane.append(inner_html)
-                    inner_data[5] = self.mile_widziane
+                    inner_data[11] = self.mile_widziane
                 except NoSuchElementException:
                     pass
                 # doswiadczenie?
@@ -176,27 +182,33 @@ class Bot:
                         text = paragraf.find_element(By.TAG_NAME, 'p')
                         inner_html = text.get_attribute("innerHTML")
                         # Prawdopodobnie trzeba przędzie patrzeć czy w jakimś paragrawfie jest x słowo/a z naszego zbioru i jesli jest to pobrac i jakos to zformatowac do jednego słowa lub cyfry na paragraf
+                        #TODO: FINISH THIS SHIT
                 except NoSuchElementException:
                     pass
                 #
                 #
                 # PAIN
-                
+                self.dane_oferty.append(inner_data)
         del self.linki_do_oferty[:]
 
     def go_to_next_site(self):
         self.current_site += 1
         self.bot.get("https://it.pracuj.pl/?pn=" + str(self.current_site))
         self.get_site_ready()
-            
+
 bot = Bot()
 bot.get_site_ready()
 bot.click_button_acc()
 numer_stron_sesji = bot.get_all_sites_nums()
-print("tytul,company,location,management_level,Pdate,tagi[tablica],salary,self.id_oferty")
+print("title, company, location, management_level, salary, tryb_pracy, etat, kontrakt, specjalizacja, technologie_wymagane[LISTA], technologie_mile_widziane[LISTA],") #Will be more data later
 while int(bot.current_site) < int(numer_stron_sesji):
     bot.get_data()
-    for dane in bot.dane_oferty:
-        print(dane)
+    
+    #print data every like 19 offers
+    for inner_list in bot.dane_oferty:
+        for item in inner_list:
+            print(item, end=' ')
+        print()
+
     bot.go_to_next_site()
-input(" ")
+#input(" ")
